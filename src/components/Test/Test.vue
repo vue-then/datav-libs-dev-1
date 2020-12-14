9835b82dc624d5adbc3a3fec783c44799804c342
yyccQQu <643929860@qq.com>
2020-12-14 15:16:56 +0400

    1

--- src/components/Test/Test.vue
+++ src/components/Test/Test.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="test">
-    <div>message: {{message}}</div>
-    <div>doubleCount: {{doubleCount}}</div>
-    <button @click="add">add count</button>
+    <svg width="100px" height="100px">
+      <line x1="0" y1="0" x2="100" y2="100" stroke-width="8" stroke="currentColor"  />
+      <line x1="0" y1="50" x2="100" y2="50" stroke-width="8" stroke="currentColor"  />
+      <line x1="50" y1="0" x2="50" y2="100" stroke-width="8" stroke="currentColor"  />
+    </svg>
+
+    <svg width="20px" height="20px" viewBox="0 0 100 100">
+      <line x1="0" y1="50" x2="100" y2="50" stroke-width="8" stroke="currentColor"  />
+      <line x1="50" y1="0" x2="50" y2="100" stroke-width="8" stroke="currentColor"  />
+    </svg>
+
+    <svg width="500" height="200" viewBox="0 0 50 20" style="border: 1px solid #000000">
+      <rect x="20" y="10" width="10" height="5" style="stroke: #000000; fill:none;"/>
+    </svg>  
+    
+    <!-- xMaxYMin meet/xMinYMin meet/xMidYMax meet -->
+    <svg width="500" height="200" viewBox="0 0 200 200" style="border: 1px solid #000000" preserveAspectRatio="xMidYMid meet">
+      <rect x="100" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill:none;"/>
+    </svg>
+
   </div>
   
 </template>
@@ -12,16 +29,10 @@ import {computed, ref} from 'vue'
 export default {
   name: 'TestComponent',
   setup() {
-    const message = 'hello world'
-    const count = ref(1)
-    const doubleCount = computed(() => count.value * 2)
-    const add = ()=>{
-      count.value++
-    }
+    let currentColor = "red";
+
     return {
-      message,
-      doubleCount,
-      add
+      currentColor
     }
   }
 }
